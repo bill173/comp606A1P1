2bb8a821961c0d69076ed1a4fe828e9b08b347f3
Bill Liu <nzbillliu@gmail.com>
2019-08-11 17:35:55 +1200

    merge login files into one

--- login.php
+++ login.php
@@ -1,7 +1,3 @@
-<?php
-require("loginserv.php");//Include loginserv for checking username and password
-?>
-
 <!doctype html>
 <html>
 <head>
@@ -9,14 +5,14 @@ require("loginserv.php");//Include loginserv for checking username and password
 <title>Login</title>
 <style>
 .login{
-    width:360px;
+    width:400px;
     margin:50px auto;
     border-radius:10px;
     padding:10px 40px 25px;
     margin-top:70px;
 }
 input[type=text], input[type=password]{
-    width:99%;
+    width:95%;
     padding:10px;
     margin-top:8px;
     padding-left:5px;
@@ -29,16 +25,48 @@ input[type=submit]{
     padding:6px;
     font-size:20px;
     cursor:pointer;
-    border-radius:5px;
+    border-radius:10px;
 }
 body {background-color: powderblue;}
 h1   {color: blue;}
 </style>
 
+<?php
+$error='';//variable to store error message
+if(isset($_POST['submit'])){
+    if(empty($_POST['user']) || empty($_POST['pass'])){
+    $error = "Username or Password is Invalid";
+    }
+    else
+    {
+        //define $user and $pass
+        $user=$_POST['user'];
+        $pass=$_POST['pass'];
+        //establishing connection with server by passing server_name, user_id and pass as a parameter
+        $conn = mysqli_connect("localhost", "bill", "1234");
+        //selecting database
+        $db = mysqli_select_db($conn, "login_register");
+        //sql query to fetch information of registered user and find user match.
+        $query = mysqli_query($conn, "SELECT * FROM userpass WHERE pass='$pass' AND user='$user'");
+
+        $rows = mysqli_num_rows($query);
+        if($rows == 1){
+            header("Location: welcome.php");//redircting to other page
+        }
+        else
+        {
+            $error = "Username or Password is Invalid.";
+        }
+        mysqli_close($conn);//closing connection
+    }
+}
+
+?>
+
 </head>
 <body>
 <div class="login">
-<h1 align="center">Hello</h1>
+<h1 align="center">User Login</h1>
 <form action="" method="post">
 <input type="text" placeholder="Username" id="user" name="user"><br/><br/>
 <input type="password" placeholder="Password" id="pass" name="pass"><br/><br/>
